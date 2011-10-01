abcba85615c4f1cb69a7f9500a1c01e16698b9ba
Guy Van Overtveldt <guyvo@Guy-Van-Overtveldts-iMac.local>
2011-10-01 21:52:48 +0200

    initial

---
+++ 3dworld/3dworld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  3dworld
+//
+//  Created by Guy Van Overtveldt on 01/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    return 0;
+}
+
